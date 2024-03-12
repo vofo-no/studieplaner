684fa35f0712906fcde20155c1162bf72419f1cf
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2024-03-12 14:19:48 +0100

    feat: add cron job

---
+++ app/api/reindex/route.ts
@@ -0,0 +1,26 @@
+import { fetchStudieplansForIndex } from "@/lib/eapply";
+import algoliasearch from "algoliasearch";
+
+import type { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
+  const data = await fetchStudieplansForIndex("skt");
+
+  const client = algoliasearch(
+    process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!,
+    process.env.ALGOLIA_ADMIN_KEY!
+  );
+
+  const index = client.initIndex("studieplaner");
+
+  index.replaceAllObjects(data).wait();
+
+  return Response.json({ updated: data.length });
+}
